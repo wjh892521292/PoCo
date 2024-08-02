c6e19cdadf0f8092bdde55a586d5027b9da6d05b
wjh892521292 <34273322+wjh892521292@users.noreply.github.com>
2024-08-02 17:07:49 +0800

    Update __init__.py

--- utils/data_load/__init__.py
+++ utils/data_load/__init__.py
@@ -1,6 +1,3 @@
-from .data1 import MyDataset as cataract_c
-from .data2 import MyDataset as cataract_p
-from .data3 import MyDataset as dr
 from .data4 import MyDataset as dr_pretrain
 from .data5_amd import MyDataset as amd
 from .data6_pm import MyDataset as pm
